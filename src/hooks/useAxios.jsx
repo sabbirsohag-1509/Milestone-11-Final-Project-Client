573191fbd22fbdab61f11f68a6fa1d10a2a87a31
sabbirsohag-1509 <sabbirhossainsohag1509@gmail.com>
2025-11-29 10:33:09 +0600

    after deploying

--- src/hooks/useAxios.jsx
+++ src/hooks/useAxios.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const useAxios = () => {
     const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://zap-shift-hazel.vercel.app",
 });
     return axiosInstance;
 };
--- src/pages/Dashboard/DashboardHome/RiderDashboardHome.jsx
+++ src/pages/Dashboard/DashboardHome/RiderDashboardHome.jsx
@@ -1,11 +1,74 @@
-import React from 'react';
+import { useQuery } from "@tanstack/react-query";
+import React from "react";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import useAuth from "../../../hooks/useAuth";
+import { PieChart, Pie, Cell, Tooltip, Legend } from "recharts";
 
-const RiderDashboardHome = () => {
-    return (
-        <div>
-            <h2 className='text-3xl font-bold p-5'>Rider Dashboard Home</h2>
+const COLORS = ["#8884d8", "#82ca9d", "#ffc658", "#ff8042", "#0088fe"];
+
+const RiderDailyDelivery = () => {
+  const axiosSecure = useAxiosSecure();
+  const { user } = useAuth();
+
+  const today = new Date().toISOString().split("T")[0];
+
+  const { data: perDayStats = [], isLoading } = useQuery({
+    queryKey: ["rider-per-day", user?.email],
+    queryFn: async () => {
+      const res = await axiosSecure.get(
+        `/riders/delivery/per-day?email=${user?.email}&date=${today}`
+      );
+      return res.data;
+    },
+  });
+
+  const chartData = perDayStats.map((stat) => ({
+    name: stat._id,
+    value: stat.count,
+  }));
+
+  return (
+    <div className="bg-base-100 rounded-2xl shadow-xl p-6 border max-w-xl mx-auto">
+      <h3 className="text-xl font-semibold text-center mb-4">
+        Rider Daily Delivery Count
+      </h3>
+
+      {/* LOADING UI */}
+      {isLoading && (
+        <p className="text-center text-sm text-gray-400">Loading…</p>
+      )}
+
+      {/* NO DATA UI */}
+      {!isLoading && chartData.length === 0 && (
+        <p className="text-center text-sm text-red-500">
+          No delivery found for today.
+        </p>
+      )}
+
+      {/* PIE CHART */}
+      {chartData.length > 0 && (
+        <div className="flex justify-center mt-4">
+          <PieChart width={350} height={350}>
+            <Pie
+              data={chartData}
+              cx="50%"
+              cy="50%"
+              outerRadius={120}
+              label
+              dataKey="value"
+            >
+              {chartData.map((_, index) => (
+                <Cell key={index} fill={COLORS[index % COLORS.length]} />
+              ))}
+            </Pie>
+
+            <Legend />
+            <Tooltip />
+          </PieChart>
         </div>
-    );
+      )}
+    </div>
+  );
 };
 
-export default RiderDashboardHome;
+export default RiderDailyDelivery;
